01ef47ecbbbe5b388fb2b147f4b062c5499e27b2
Bonjing <johnemilbragais@gmail.com>
2025-06-14 15:10:09 +0800

    added signup page

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+
+Route::post('/signup', function () {
+    // Handle signup form submission here
+})->name('signup.submit');
