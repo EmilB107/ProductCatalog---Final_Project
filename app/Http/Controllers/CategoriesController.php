b1c8ada794f6ab584053742f3578ee0dc03a5f5e
Bonjing <johnemilbragais@gmail.com>
2025-06-16 14:33:07 +0800

    Update CategoriesController.php

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -12,14 +12,9 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        // Fetch all categories from the database
-        // We'll also eager load the category and subCategory relationships
-        // to avoid N+1 query problem.
         $categories = Categories::with(['subCategories', 'products'])->get();
 
-        // Pass the categories to a view
+
         return view('categories.index', compact('categories'));
     }
-
-    // You can add other methods like show, create, store, edit, update, destroy later
 }
