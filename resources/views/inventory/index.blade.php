57aad8a2ee9cb26bd9185009b7f632bbdec2b267
Bonjing <johnemilbragais@gmail.com>
2025-06-18 12:48:56 +0800

    Update index.blade.php

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -1,4 +1,3 @@
-{{-- filepath: resources/views/inventory/index.blade.php --}}
 @extends('layouts.dashboard-layout')
 
 @section('title', 'Inventory')
@@ -41,6 +40,11 @@
                     @endfor
                 </tbody>
             </table>
+        </div>
+        <div class="mt-2 text-end">
+            <a href="{{ route('inventory.update') }}" role="button" class="add-btn">
+                Update Inventory
+            </a>
         </div>
     </section>
 @endsection
