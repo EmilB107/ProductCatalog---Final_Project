ffe347ec9357236ede24f26672508f6424d75ca1
Bonjing <johnemilbragais@gmail.com>
2025-06-19 20:17:38 +0800

    Update _return.blade.php

--- resources/views/partials/_return.blade.php
+++ resources/views/partials/_return.blade.php
@@ -1,4 +1,4 @@
 <div class="return d-flex">
     <img src="{{ asset('images/return.png') }}" alt="Return">
-    <a href="{{ route('products.index') }}">Back</a>
+    <a href="{{ route($route) }}">Return</a>
 </div>
