01ed4cc3b319e3683eb5d5387d0cd905682d0b7a
Bonjing <johnemilbragais@gmail.com>
2025-06-19 20:18:15 +0800

    Update update.blade.php

--- resources/views/inventory/update.blade.php
+++ resources/views/inventory/update.blade.php
@@ -5,54 +5,48 @@
 @section('content')
     <section class="d-inventory-update">
         <div class="d-flex mb-3">
-            @include('partials._return')
+            @include('partials._return', ['route' => 'inventory.index'])
             <h1 class="mx-auto">Update Inventory</h1>
         </div>
         <div class="table-responsive">
-            <table class="table table-bordered text-center align-middle">
-                <colgroup>
-                    <col style="width: 18%;">   <!-- Product Name -->
-                    <col style="width: 14%;">   <!-- Image -->
-                    <col style="width: 14%;">   <!-- SKU -->
-                    <col style="width: 14%;">   <!-- Quantity (smaller) -->
-                    <col style="width: 24%;">   <!-- Stock (bigger) -->
-                    <col style="width: 16%;">   <!-- Action (bigger) -->
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th>Product Name</th>
-                        <th>Image</th>
-                        <th>SKU</th>
-                        <th>Quantity</th>
-                        <th>Stock</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @for ($i = 0; $i < 3; $i++)
+            <form>
+                <table class="table table-bordered text-center align-middle">
+                    <thead>
                         <tr>
-                            <td>Sample Product</td>
-                            <td class="img-col">
-                                <img src="{{ asset('images/img-placeholder.png') }}" alt="Product Image">
-                            </td>
-                            <td>SKU-001</td>
-                            <td>
-                                <input type="number" class="form-control text-center" value="55" min="0">
-                            </td>
-                            <td>
-                                <select class="form-select text-center">
-                                    <option>Low Stock</option>
-                                    <option>In Stock</option>
-                                    <option>Out of Stock</option>
-                                </select>
-                            </td>
-                            <td>
-                                <a href="#" onclick="this.closest('form').submit(); return false;">Save</a>
-                            </td>
+                            <th>Product Name</th>
+                            <th>Image</th>
+                            <th>SKU</th>
+                            <th>Quantity</th>
+                            <th>Stock</th>
+                            <th>Action</th>
                         </tr>
-                    @endfor
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($inventory as $item)
+                            <tr>
+                                <td>{{ $item['name'] }}</td>
+                                <td>
+                                    <img src="{{ asset($item['image']) }}" alt="Product Image" width="40">
+                                </td>
+                                <td>{{ $item['sku'] }}</td>
+                                <td>
+                                    <input type="number" class="form-control text-center" value="{{ $item['quantity'] }}">
+                                </td>
+                                <td>
+                                    <select class="form-select text-center">
+                                        <option {{ $item['stock'] == 'Low Stock' ? 'selected' : '' }}>Low Stock</option>
+                                        <option {{ $item['stock'] == 'In Stock' ? 'selected' : '' }}>In Stock</option>
+                                        <option {{ $item['stock'] == 'Out of Stock' ? 'selected' : '' }}>Out of Stock</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <a href="#" onclick="this.closest('form').submit(); return false;">Save</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </form>
         </div>
     </section>
 @endsection
