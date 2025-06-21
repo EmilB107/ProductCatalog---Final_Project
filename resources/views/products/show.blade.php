6fdf3de0ad048e36c74488f4d8ae56a837d92bea
Bonjing <johnemilbragais@gmail.com>
2025-06-22 00:15:50 +0800

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,23 +10,23 @@
         </div>
         <div class="crud-card d-flex gap-4 align-items-start mx-auto">
             <div>
-                <h2>Product Name</h2>
+                <h2>{{ $product['name'] }}</h2>
                 <div class="mt-3">
-                    <div><b>SKU</b> &nbsp; <span>Lorem Ipsum</span></div>
-                    <div class="mt-2"><b>Description</b> &nbsp; <span>Lorem Ipsum</span></div>
-                    <div class="d-flex flex-column flex-lg-row gap-lg-4">
-                        <div class="mt-2"><b>Category</b> &nbsp; <span>Lorem Ipsum</span></div>
-                        <div class="mt-2"><b>Sub Category</b> &nbsp; <span>Lorem Ipsum</span></div>
+                    <div class="text"><b>SKU</b> &nbsp; <span>{{ $product['sku'] }}</span></div>
+                    <div class="text d-flex flex-column mt-2"><b>Description</b><span class="desc">{{ $product['description'] }}</span></div>
+                    <div class="text d-flex flex-column flex-lg-row gap-lg-4">
+                        <div class="text mt-2"><b>Category</b><span class="d-block">{{ $product['category'] }}</span></div>
+                        <div class="text mt-2"><b>Sub Category</b><span class="d-block">{{ $product['subcategory'] }}</span></div>
                     </div>
-                    <div class="mt-2"><b>Price</b> &nbsp; <span>Lorem Ipsum</span></div>
-                    <div class="mt-2"><b>Stock</b> &nbsp; <span>#</span></div>
+                    <div class="text mt-2"><b>Price</b> &nbsp; <span>{{ $product['price'] }}</span></div>
+                    <div class="text mt-2"><b>Stock</b> &nbsp; <span>{{ $product['stock'] }}</span></div>
                 </div>
             </div>
-            <div class="d-flex flex-column align-items-center">
-                <img src="{{ asset('images/img-placeholder.png') }}" alt="Product Image">
+            <div class="d-flex flex-column align-items-center ms-auto">
+                <img src="{{ asset($product['image']) }}" alt="Product Image">
                 <div class="mt-auto">
-                    <p class="mt-3 mb-2 text-center">Low Stock</p>
-                    <a href="{{ route('products.edit', 1) }}">
+                    <p class="text-center">{{ $product['stock'] <= 5 ? 'Low Stock' : 'In Stock' }}</p>
+                    <a href="{{ route('products.edit', $product['id']) }}">
                         Edit this Product
                     </a>
                 </div>
